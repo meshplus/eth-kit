892d717aa66b731355765c9030c33110b67a0763
jzhe <jz570889446@gmail.com>
2021-08-13 10:18:22 +0800

    chore(ledger):remove prepareEVM log

--- ledger/evmstate_accessor.go
+++ ledger/evmstate_accessor.go
@@ -132,7 +132,6 @@ func (l *ComplexStateLedger) AddEVMPreimage(hash common.Hash, data []byte) {
 
 func (l *ComplexStateLedger) PrepareEVM(hash common.Hash, index int) {
 	l.thash = types.NewHash(hash.Bytes())
-	l.logger.Info("PrepareEVM: " + l.thash.String())
 	l.txIndex = index
 	l.accessList = NewAccessList()
 }
